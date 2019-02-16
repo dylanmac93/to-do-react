d399c4715c0987cdc454e2902554a7b88534a92d
dmacnamee <dmacnamee@cartrawler.com>
2019-02-16 16:12:07 +0000

    created header component, restyled app.js, turned service worker on

---
+++ to-do-app/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="Header">
+      <div className="header-container">To Do List</div>
+    </div>
+  );
+};
+
+export default Header;
